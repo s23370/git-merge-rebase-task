7d26a4fe8f61eb0c1f160062150e945363382e0d
Jan Boroń <jan.boron2001@gmail.com>
2025-06-22 00:00:56 +0200

    Add CalculateAverage method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!!!");
+﻿
+int[] numbers = { 2, 4, 6, 8, 10 };
+double avg = CalculateAverage(numbers);
+Console.WriteLine($"Średnia: {avg}");
+
+
+static double CalculateAverage(int[] numbers)
+{
+    if (numbers == null || numbers.Length == 0)
+        return 0;
+
+    int sum = 0;
+    foreach (int num in numbers)
+        sum += num;
+
+    return (double)sum / numbers.Length;
+}
